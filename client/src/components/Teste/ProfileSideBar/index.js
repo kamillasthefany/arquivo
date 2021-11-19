4400d9d3b639f981ee35687ed3a6c2cdc4bd4ac5
Kamilla <kamilla.rodrigues@auvo.com.br>
2021-11-19 12:26:57 -0300

    user icon

--- client/src/components/Teste/ProfileSideBar/index.js
+++ client/src/components/Teste/ProfileSideBar/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Box from '../Box';
 import { ProfileSidebarMenu } from '../ProfileSidebarMenu';
+import userFoto from '../../../assets/user.png';
 
 export function ProfileSideBar(props) {
   return (
     <Box>
-      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '5px' }} />
+      <img src={userFoto} style={{ borderRadius: '5px' }} />
       <hr />
       <p>
-        <a className="boxLink" href={`https://github/${props.githubUser}`} >
+        @usuario
+        {/* <a className="boxLink" href={`https://github/${props.githubUser}`} >
           @{props.githubUser}
-        </a>
+        </a> */}
       </p>
       <hr />
       <ProfileSidebarMenu />
